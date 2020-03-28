ae69ca7d6d2b2d83a51d5a1c007e87ce3578683e
Dcrew <dean.reynolds@live.co.uk>
2020-03-28 15:06:54 +0000

    add docu

--- src/Quadtree.cs
+++ src/Quadtree.cs
@@ -267,6 +267,7 @@ namespace Dcrew.MonoGame._2D_Spatial_Partition
             }
         }
 
+        /// <summary>Set the boundary rect of this tree</summary>
         public static Rectangle Bounds
         {
             get => _mainNode.Bounds;
@@ -302,6 +303,7 @@ namespace Dcrew.MonoGame._2D_Spatial_Partition
 
         /// <summary>Returns true if <paramref name="item"/> is in the tree</summary>
         public static bool Contains(T item) => _stored.ContainsKey(item);
+        /// <summary>Return all items and their container rects</summary>
         public static IEnumerable<(T Item, Rectangle Node)> Items
         {
             get
