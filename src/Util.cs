2e4d778e826e6c0367a3786b14203809bbce971d
Dcrew <dean.reynolds@live.co.uk>
2020-04-07 22:30:48 +0100

    extracted out rotate method into Util class

---
+++ src/Util.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dcrew.MonoGame._2D_Spatial_Partition
+{
+    static class Util
+    {
+        /// <summary>Returns an axis-aligned rectangle covering <paramref name="area"/> rotated by <paramref name="angle"/> using the given offset <paramref name="origin"/></summary>
+        /// <param name="area">Area (rectangle)</param>
+        /// <param name="angle">Rotation (in radians) of <paramref name="area"/></param>
+        /// <param name="origin">Origin of <paramref name="area"/></param>
+        public static Rectangle Rotate(Rectangle area, float angle, Vector2 origin)
+        {
+            float cos = MathF.Cos(angle),
+                sin = MathF.Sin(angle),
+                x = -origin.X,
+                y = -origin.Y,
+                w = area.Width + x,
+                h = area.Height + y,
+                xcos = x * cos,
+                ycos = y * cos,
+                xsin = x * sin,
+                ysin = y * sin,
+                wcos = w * cos,
+                wsin = w * sin,
+                hcos = h * cos,
+                hsin = h * sin,
+                tlx = xcos - ysin,
+                tly = xsin + ycos,
+                trx = wcos - ysin,
+                tr_y = wsin + ycos,
+                brx = wcos - hsin,
+                bry = wsin + hcos,
+                blx = xcos - hsin,
+                bly = xsin + hcos,
+                minx = tlx,
+                miny = tly,
+                maxx = minx,
+                maxy = miny;
+            if (trx < minx)
+                minx = trx;
+            if (brx < minx)
+                minx = brx;
+            if (blx < minx)
+                minx = blx;
+            if (tr_y < miny)
+                miny = tr_y;
+            if (bry < miny)
+                miny = bry;
+            if (bly < miny)
+                miny = bly;
+            if (trx > maxx)
+                maxx = trx;
+            if (brx > maxx)
+                maxx = brx;
+            if (blx > maxx)
+                maxx = blx;
+            if (tr_y > maxy)
+                maxy = tr_y;
+            if (bry > maxy)
+                maxy = bry;
+            if (bly > maxy)
+                maxy = bly;
+            return new Rectangle((int)minx, (int)miny, (int)MathF.Ceiling(maxx - minx), (int)MathF.Ceiling(maxy - miny));
+        }
+    }
+}
