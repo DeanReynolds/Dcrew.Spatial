7d790d8dd1ac72aa42eef3c8144cd56b282cc0c0
Dcrew <dean.reynolds@live.co.uk>
2020-08-21 23:46:34 +0100

    change rotrect algorithm (fixing all bugs)

--- src/RotRect.cs
+++ src/RotRect.cs
@@ -39,85 +39,47 @@ namespace Dcrew.Spatial
 
         bool IntersectsAnyEdge(RotRect rectangle)
         {
+            static float IsLeft(Vector2 a, Vector2 b, Vector2 p) => (b.X - a.X) * (p.Y - a.Y) - (p.X - a.X) * (b.Y - a.Y);
+            static bool PointInRectangle(Vector2 x, Vector2 y, Vector2 z, Vector2 w, Vector2 p) => (IsLeft(x, y, p) > 0 && IsLeft(y, z, p) > 0 && IsLeft(z, w, p) > 0 && IsLeft(w, x, p) > 0);
             Vector2 center = Util.Rotate(Rect, Angle, Origin).Center.ToVector2(),
-                closest,
-                tl,
-                tr,
-                br,
-                bl;
-            float cos = MathF.Cos(rectangle.Angle),
-                sin = MathF.Sin(rectangle.Angle),
-                x = -rectangle.Origin.X,
-                y = -rectangle.Origin.Y,
-                w = rectangle.Rect.Width + x,
-                h = rectangle.Rect.Height + y,
-                xcos = x * cos,
-                ycos = y * cos,
-                xsin = x * sin,
-                ysin = y * sin,
-                wcos = w * cos,
-                wsin = w * sin,
-                hcos = h * cos,
-                hsin = h * sin,
-                tlx = xcos - ysin + rectangle.Rect.X,
-                tly = xsin + ycos + rectangle.Rect.Y,
-                trx = wcos - ysin + rectangle.Rect.X,
-                tr_y = wsin + ycos + rectangle.Rect.Y,
-                brx = wcos - hsin + rectangle.Rect.X,
-                bry = wsin + hcos + rectangle.Rect.Y,
-                blx = xcos - hsin + rectangle.Rect.X,
-                bly = xsin + hcos + rectangle.Rect.Y;
-            if (Vector2.DistanceSquared(tl = new Vector2(tlx, tly), center) < Vector2.DistanceSquared(tr = new Vector2(trx, tr_y), center))
-                closest = tl;
-            else
-                closest = tr;
-            if (Vector2.DistanceSquared(br = new Vector2(brx, bry), center) < Vector2.DistanceSquared(closest, center))
-                closest = br;
-            if (Vector2.DistanceSquared(bl = new Vector2(blx, bly), center) < Vector2.DistanceSquared(closest, center))
-                closest = bl;
-            if (closest == tl)
-            {
-                Vector2 a = new Line(tl, bl).ClosestPoint(center),
-                    b = new Line(tl, tr).ClosestPoint(center);
-                if (Vector2.DistanceSquared(a, center) < Vector2.DistanceSquared(b, center))
-                    closest = a;
-                else
-                    closest = b;
-            }
-            else if (closest == tr)
-            {
-                Vector2 a = new Line(tr, tl).ClosestPoint(center),
-                    b = new Line(tr, br).ClosestPoint(center);
-                if (Vector2.DistanceSquared(a, center) < Vector2.DistanceSquared(b, center))
-                    closest = a;
-                else
-                    closest = b;
-            }
-            else if (closest == br)
-            {
-                Vector2 a = new Line(br, tr).ClosestPoint(center),
-                    b = new Line(br, bl).ClosestPoint(center);
-                if (Vector2.DistanceSquared(a, center) < Vector2.DistanceSquared(b, center))
-                    closest = a;
-                else
-                    closest = b;
-            }
-            else
-            {
-                Vector2 a = new Line(bl, tl).ClosestPoint(center),
-                    b = new Line(bl, br).ClosestPoint(center);
-                if (Vector2.DistanceSquared(a, center) < Vector2.DistanceSquared(b, center))
-                    closest = a;
-                else
-                    closest = b;
-            }
-            var rect = Rect;
-            rect.Offset(-Origin);
-            cos = MathF.Cos(-Angle);
-            sin = MathF.Sin(-Angle);
-            float cx = closest.X - Rect.X,
-                cy = closest.Y - Rect.Y;
-            return rect.Contains(new Vector2(cx * cos + cy * -sin + Rect.X, cx * sin + cy * cos + Rect.Y));
+                closest;
+            float cos = MathF.Cos(Angle),
+             sin = MathF.Sin(Angle),
+             x = -Origin.X,
+             y = -Origin.Y,
+             w = Rect.Width + x,
+             h = Rect.Height + y,
+             xcos = x * cos,
+             ycos = y * cos,
+             xsin = x * sin,
+             ysin = y * sin,
+             wcos = w * cos,
+             wsin = w * sin,
+             hcos = h * cos,
+             hsin = h * sin;
+            Vector2 tl2 = new Vector2(xcos - ysin + Rect.X, xsin + ycos + Rect.Y),
+             tr2 = new Vector2(wcos - ysin + Rect.X, wsin + ycos + Rect.Y),
+             br2 = new Vector2(wcos - hsin + Rect.X, wsin + hcos + Rect.Y),
+             bl2 = new Vector2(xcos - hsin + Rect.X, xsin + hcos + Rect.Y);
+            cos = MathF.Cos(rectangle.Angle);
+            sin = MathF.Sin(rectangle.Angle);
+            x = -rectangle.Origin.X;
+            y = -rectangle.Origin.Y;
+            w = rectangle.Rect.Width + x;
+            h = rectangle.Rect.Height + y;
+            xcos = x * cos;
+            ycos = y * cos;
+            xsin = x * sin;
+            ysin = y * sin;
+            wcos = w * cos;
+            wsin = w * sin;
+            hcos = h * cos;
+            hsin = h * sin;
+            Vector2 tl = new Vector2(xcos - ysin + rectangle.Rect.X, xsin + ycos + rectangle.Rect.Y),
+             tr = new Vector2(wcos - ysin + rectangle.Rect.X, wsin + ycos + rectangle.Rect.Y),
+             br = new Vector2(wcos - hsin + rectangle.Rect.X, wsin + hcos + rectangle.Rect.Y),
+             bl = new Vector2(xcos - hsin + rectangle.Rect.X, xsin + hcos + rectangle.Rect.Y);
+            return PointInRectangle(tl2, tr2, br2, bl2, new Line(tl, tr).ClosestPoint(center)) || PointInRectangle(tl2, tr2, br2, bl2, new Line(tr, br).ClosestPoint(center)) || PointInRectangle(tl2, tr2, br2, bl2, new Line(br, bl).ClosestPoint(center)) || PointInRectangle(tl2, tr2, br2, bl2, new Line(bl, tl).ClosestPoint(center));
         }
     }
 }
