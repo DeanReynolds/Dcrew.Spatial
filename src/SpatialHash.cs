e4f7564109b232dbf5c0138897ec739260621c88
Dcrew <dean.reynolds@live.co.uk>
2020-09-09 19:59:20 +0100

    clean spatialhash

--- src/SpatialHash.cs
+++ src/SpatialHash.cs
@@ -3,31 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Dcrew.Spatial
-{
+namespace Dcrew.Spatial {
     /// <summary>For very fast but approximate spatial partitioning. See <see cref="Spacing"/> before use</summary>
-    public class SpatialHash<T> : IEnumerable<T> where T : class, IAABB
-    {
+    public class SpatialHash<T> : IEnumerable<T> where T : class, IAABB {
         const int DEFAULT_SPACING = 50;
 
         /// <summary>Set to your largest item collision radius. Default: 50</summary>
-        public int Spacing
-        {
+        public int Spacing {
             get => _spacing;
-            set
-            {
+            set {
                 _halfSpacing = (_spacing = value) / 2;
                 var bundles = _item.ToArray();
                 foreach (var (_, n) in bundles)
-                    if (_hash.ContainsKey(n))
-                    {
+                    if (_hash.ContainsKey(n)) {
                         var t = _hash[n];
                         t.Clear();
                         Pool<HashSet<T>>.Free(t);
                         _hash.Remove(n);
                     }
-                foreach (var (i, _) in bundles)
-                {
+                foreach (var (i, _) in bundles) {
                     var b = Bucket(i);
                     Add(i, b);
                     _item[i] = b;
@@ -42,10 +36,8 @@ namespace Dcrew.Spatial
         /// <summary>Return count of all items</summary>
         public int ItemCount => _item.Count;
         /// <summary>Return all items and their container points</summary>
-        public IEnumerable<(T Item, Vector2 Node)> Bundles
-        {
-            get
-            {
+        public IEnumerable<(T Item, Vector2 Node)> Bundles {
+            get {
                 foreach (var i in _item)
                     yield return (i.Key, new Vector2(i.Value.X * _spacing + _halfSpacing, i.Value.Y * _spacing + _halfSpacing));
             }
@@ -61,8 +53,7 @@ namespace Dcrew.Spatial
         public SpatialHash(int spacing = DEFAULT_SPACING) => _halfSpacing = (_spacing = spacing) / 2;
 
         /// <summary>Inserts <paramref name="item"/> into the tree. ONLY USE IF <paramref name="item"/> ISN'T ALREADY IN THE TREE</summary>
-        public void Add(T item)
-        {
+        public void Add(T item) {
             var bucket = Bucket(item);
             Add(item, bucket);
             _item.Add(item, bucket);
@@ -70,46 +61,38 @@ namespace Dcrew.Spatial
         }
         /// <summary>Updates <paramref name="item"/>'s position in the tree. ONLY USE IF <paramref name="item"/> IS ALREADY IN THE TREE</summary>
         /// <returns>True if <paramref name="item"/> was moved to a new bucket, false otherwise</returns>
-        public bool Update(T item)
-        {
+        public bool Update(T item) {
             Point bucket = Bucket(item),
                 i = _item[item];
             if (bucket == i)
                 return false;
             var t = _hash[i];
-            if (t.Count <= 1)
-            {
+            if (t.Count <= 1) {
                 t.Clear();
                 Pool<HashSet<T>>.Free(t);
                 _hash.Remove(i);
-            }
-            else
+            } else
                 t.Remove(item);
             Add(item, bucket);
             _item[item] = bucket;
             return true;
         }
         /// <summary>Removes <paramref name="item"/> from the tree. ONLY USE IF <paramref name="item"/> IS ALREADY IN THE TREE</summary>
-        public void Remove(T item)
-        {
+        public void Remove(T item) {
             var i = _item[item];
             var t = _hash[i];
-            if (t.Count <= 1)
-            {
+            if (t.Count <= 1) {
                 t.Clear();
                 Pool<HashSet<T>>.Free(t);
                 _hash.Remove(i);
-            }
-            else
+            } else
                 t.Remove(item);
             _item.Remove(item);
             _item2.Remove(item);
         }
         /// <summary>Removes all items and buckets from the tree</summary>
-        public void Clear()
-        {
-            foreach (var t in _hash.Values)
-            {
+        public void Clear() {
+            foreach (var t in _hash.Values) {
                 t.Clear();
                 Pool<HashSet<T>>.Free(t);
             }
@@ -121,8 +104,7 @@ namespace Dcrew.Spatial
         /// <summary>Query and return the items intersecting <paramref name="xy"/></summary>
         public IEnumerable<T> Query(Vector2 xy) => InQuery(new Point((int)(xy.X / Spacing), (int)(xy.Y / Spacing)));
         /// <summary>Query and return the items intersecting <paramref name="area"/></summary>
-        public IEnumerable<T> Query(Rectangle area)
-        {
+        public IEnumerable<T> Query(Rectangle area) {
             int x = area.X / Spacing,
                 y = area.Y / Spacing;
             int lX = area.Width / Spacing + x + 1,
@@ -133,8 +115,7 @@ namespace Dcrew.Spatial
                         if (t.AABB.Intersects(area))
                             yield return t;
             int e = area.Width / Spacing % 3;
-            if (e > 0)
-            {
+            if (e > 0) {
                 for (var k = y; k < lY; k += 3)
                     foreach (var t in InQuery(new Point(e % 2 + lX, k)))
                         if (t.AABB.Intersects(area))
@@ -152,24 +133,20 @@ namespace Dcrew.Spatial
         /// <param name="area">Area (rectangle)</param>
         /// <param name="angle">Rotation (in radians) of <paramref name="area"/></param>
         /// <param name="origin">Origin of <paramref name="area"/></param>
-        public IEnumerable<T> Query(Rectangle area, float angle, Vector2 origin)
-        {
+        public IEnumerable<T> Query(Rectangle area, float angle, Vector2 origin) {
             area = Util.Rotate(area, angle, origin);
             foreach (var t in Query(area))
                 yield return t;
         }
 
-        internal Point Bucket(T item)
-        {
+        internal Point Bucket(T item) {
             var aabb = Util.Rotate(item.AABB, item.Angle, item.Origin);
             var pos = aabb.Center;
             return new Point(pos.X / Spacing, pos.Y / Spacing);
         }
 
-        internal void Add(T obj, Point bucket)
-        {
-            if (!_hash.ContainsKey(bucket))
-            {
+        internal void Add(T obj, Point bucket) {
+            if (!_hash.ContainsKey(bucket)) {
                 var set = Pool<HashSet<T>>.Spawn();
                 set.Add(obj);
                 _hash.Add(bucket, set);
@@ -178,8 +155,7 @@ namespace Dcrew.Spatial
             _hash[bucket].Add(obj);
         }
 
-        IEnumerable<T> InQuery(Point p)
-        {
+        IEnumerable<T> InQuery(Point p) {
             var bucket = p;
             if (_hash.ContainsKey(bucket))
                 foreach (var i in _hash[bucket])
