e6584e1d7e70e5b3081b16a520d6eba24fadb73a
Dcrew <dean.reynolds@live.co.uk>
2020-10-08 23:10:06 +0100

    ibounds docu clean

--- src/IBounds.cs
+++ src/IBounds.cs
@@ -1,5 +1,5 @@
 ﻿namespace Dcrew.Spatial {
-    /// <summary>Oriented bounding box</summary>
+    /// <summary>Oriented bounding rectangle.</summary>
     public interface IBounds {
         /// <summary>A <see cref="RotRect"/> defining the bounds of this object.</summary>
         RotRect Bounds { get; }
