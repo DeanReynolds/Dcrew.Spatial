04094f9beb9667ce6b4ce0c04df839cd71cd88d8
Dcrew <dean.reynolds@live.co.uk>
2020-10-10 21:06:59 +0100

    stackquadtree > loose

--- src/StackQuadtree.cs
+++ src/StackQuadtree.cs
@@ -9,6 +9,7 @@ namespace Dcrew.Spatial {
 
             internal int _parent, _firstChild, _itemCount, _cX, _cY;
             internal byte _depth;
+            internal Rectangle _bounds;
         }
 
         struct Item {
@@ -17,13 +18,40 @@ namespace Dcrew.Spatial {
             internal Point _xy;
         }
 
+        public IEnumerable<Rectangle> Nodes(RotRect area = default) {
+            if (area.Size == Vector2.Zero)
+                area = new RotRect(_bounds);
+            _toProcess.Push(0);
+            do {
+                var nId = _toProcess.Pop();
+                var n = _nodes[nId];
+                yield return n._bounds;
+                if (n._itemCount < 0) {
+                    var node2 = _nodes[n._firstChild];
+                    if (area.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild);
+                    node2 = _nodes[n._firstChild + 1];
+                    if (area.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild + 1);
+                    node2 = _nodes[n._firstChild + 2];
+                    if (area.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild + 2);
+                    node2 = _nodes[n._firstChild + 3];
+                    if (area.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild + 3);
+                    continue;
+                }
+            } while (_toProcess.Count > 0);
+            yield break;
+        }
+
         Rectangle _bounds;
         int _nextNodeId;
-        (int ItemId, int Size, int HalfSize) _maxRadiusItem;
 
         readonly Item[] _items;
         readonly Node[] _nodes;
         readonly Stack<int> _toProcess;
+        readonly HashSet<int> _nodesToGrow;
 
         public StackQuadtree(Rectangle bounds, int itemCapacity) {
             _bounds = bounds;
@@ -35,28 +63,22 @@ namespace Dcrew.Spatial {
             for (var i = 0; i < _items.Length; i++)
                 _items[i] = new Item { _id = i, _next = -1, _node = -1 };
             _toProcess = new Stack<int>();
-            _maxRadiusItem = (-1, 0, 0);
+            _nodesToGrow = new HashSet<int>();
         }
 
         public void Insert(int id, Vector2 xy, Vector2 size, float angle = 0, Vector2 origin = default) {
             var bounds = new RotRect(xy, size, angle, origin);
             var aabb = bounds.AABB;
-            if (aabb.Width > _maxRadiusItem.Size)
-                _maxRadiusItem = (id, aabb.Width, (int)MathF.Ceiling(aabb.Width / 2f));
-            if (aabb.Height > _maxRadiusItem.Size)
-                _maxRadiusItem = (id, aabb.Height, (int)MathF.Ceiling(aabb.Height / 2f));
             var center = aabb.Center;
-            TrySubdivide(Insert(id, 0, bounds, center));
+            int nodeId = Insert(id, 0, bounds, center);
+            TrySubdivide(nodeId);
+            _nodesToGrow.Add(nodeId);
         }
         /// <summary>Updates position of <paramref name="id"/> in the tree.</summary>
         /// <returns>True if <paramref name="id"/> has been updated, otherwise false.</returns>
         public bool Update(int id, Vector2 xy, Vector2 size, float angle = 0, Vector2 origin = default) {
             var bounds = new RotRect(xy, size, angle, origin);
             var aabb = bounds.AABB;
-            if (aabb.Width > _maxRadiusItem.Size)
-                _maxRadiusItem = (id, aabb.Width, (int)MathF.Ceiling(aabb.Width / 2f));
-            if (aabb.Height > _maxRadiusItem.Size)
-                _maxRadiusItem = (id, aabb.Height, (int)MathF.Ceiling(aabb.Height / 2f));
             var center = aabb.Center;
             var item = _items[id];
             var node = _nodes[item._node];
@@ -66,6 +88,7 @@ namespace Dcrew.Spatial {
             if (bounds2.Contains(center) || node._parent == -1) {
                 _items[id]._bounds = bounds;
                 _items[id]._xy = center;
+                _nodesToGrow.Add(item._node);
                 return true;
             }
             if (node._firstChild == id)
@@ -83,6 +106,7 @@ namespace Dcrew.Spatial {
                 }
             }
             _nodes[item._node]._itemCount--;
+            _nodesToGrow.Add(item._node);
             var nodeId = item._node;
             do {
                 if (node._parent < 0)
@@ -98,7 +122,9 @@ namespace Dcrew.Spatial {
                 nodeId = node._parent;
                 node = _nodes[nodeId];
             } while (true);
-            TrySubdivide(Insert(id, nodeId, bounds, center));
+            var nId2 = Insert(id, nodeId, bounds, center);
+            TrySubdivide(nId2);
+            _nodesToGrow.Add(nId2);
             return true;
         }
 
@@ -113,26 +139,36 @@ namespace Dcrew.Spatial {
         public IEnumerable<int> Query(Rectangle area, float angle = 0, Vector2 origin = default) => Query(new RotRect(area.Location.ToVector2(), area.Size.ToVector2(), angle, origin));
         /// <summary>Query and return the items intersecting <paramref name="rect"/>.</summary>
         public IEnumerable<int> Query(RotRect rect) {
-            var broad = rect;
-            broad.Inflate(_maxRadiusItem.HalfSize, _maxRadiusItem.HalfSize);
             _toProcess.Push(0);
             do {
-                var nodeId = _toProcess.Pop();
-                var node = _nodes[nodeId];
-                int halfWidth = _bounds.Width >> node._depth,
-                    halfHeight = _bounds.Height >> node._depth;
-                var bounds = new Rectangle(node._cX - halfWidth, node._cY - halfHeight, halfWidth << 1, halfHeight << 1);
-                if (node._itemCount > 0) {
-                    var item = _items[node._firstChild];
-                    if (rect.Contains(bounds)) {
+                var nId = _toProcess.Pop();
+                var n = _nodes[nId];
+                if (n._itemCount < 0) {
+                    var node2 = _nodes[n._firstChild];
+                    if (rect.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild);
+                    node2 = _nodes[n._firstChild + 1];
+                    if (rect.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild + 1);
+                    node2 = _nodes[n._firstChild + 2];
+                    if (rect.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild + 2);
+                    node2 = _nodes[n._firstChild + 3];
+                    if (rect.Intersects(node2._bounds))
+                        _toProcess.Push(n._firstChild + 3);
+                    continue;
+                }
+                if (n._itemCount > 0) {
+                    var item = _items[n._firstChild];
+                    if (rect.Contains(n._bounds)) {
                         //do {
                         //    yield return item._id;
                         //    if (item._next < 0)
                         //        break;
                         //    item = _items[item._next];
                         //} while (true);
-                        var itemId = node._firstChild;
-                        for (var i = 0; i < node._itemCount; i++) {
+                        var itemId = n._firstChild;
+                        for (var i = 0; i < n._itemCount; i++) {
                             item = _items[itemId];
                             yield return item._id;
                             itemId = item._next;
@@ -145,32 +181,6 @@ namespace Dcrew.Spatial {
                                 break;
                             item = _items[item._next];
                         } while (true);
-                } else if (node._itemCount < 0) {
-                    var node2 = _nodes[node._firstChild];
-                    halfWidth = _bounds.Width >> node2._depth;
-                    halfHeight = _bounds.Height >> node2._depth;
-                    bounds = new Rectangle(node2._cX - halfWidth, node2._cY - halfHeight, halfWidth << 1, halfHeight << 1);
-                    if (broad.Intersects(bounds))
-                        _toProcess.Push(node._firstChild);
-                    node2 = _nodes[node._firstChild + 1];
-                    halfWidth = _bounds.Width >> node2._depth;
-                    halfHeight = _bounds.Height >> node2._depth;
-                    bounds = new Rectangle(node2._cX - halfWidth, node2._cY - halfHeight, halfWidth << 1, halfHeight << 1);
-                    if (broad.Intersects(bounds))
-                        _toProcess.Push(node._firstChild + 1);
-                    node2 = _nodes[node._firstChild + 2];
-                    halfWidth = _bounds.Width >> node2._depth;
-                    halfHeight = _bounds.Height >> node2._depth;
-                    bounds = new Rectangle(node2._cX - halfWidth, node2._cY - halfHeight, halfWidth << 1, halfHeight << 1);
-                    if (broad.Intersects(bounds))
-                        _toProcess.Push(node._firstChild + 2);
-                    node2 = _nodes[node._firstChild + 3];
-                    halfWidth = _bounds.Width >> node2._depth;
-                    halfHeight = _bounds.Height >> node2._depth;
-                    bounds = new Rectangle(node2._cX - halfWidth, node2._cY - halfHeight, halfWidth << 1, halfHeight << 1);
-                    if (broad.Intersects(bounds))
-                        _toProcess.Push(node._firstChild + 3);
-                    continue;
                 }
             } while (_toProcess.Count > 0);
             yield break;
@@ -178,9 +188,9 @@ namespace Dcrew.Spatial {
 
         int Insert(int id, int nodeId, RotRect bounds, Point xy) {
             do {
-                var node = _nodes[nodeId];
-                if (node._itemCount < 0) {
-                    nodeId = xy.X < node._cX ? xy.Y < node._cY ? node._firstChild : node._firstChild + 3 : xy.Y < node._cY ? node._firstChild + 1 : node._firstChild + 2;
+                var n = _nodes[nodeId];
+                if (n._itemCount < 0) {
+                    nodeId = xy.X < n._cX ? xy.Y < n._cY ? n._firstChild : n._firstChild + 3 : xy.Y < n._cY ? n._firstChild + 1 : n._firstChild + 2;
                     continue;
                 }
                 ForceInsert(id, nodeId, bounds, xy);
@@ -241,15 +251,71 @@ namespace Dcrew.Spatial {
                 var itemId = node._firstChild;
                 for (var i = 0; i < node._itemCount; i++) {
                     var item = _items[itemId];
-                    int nodeId2;
-                    nodeId2 = item._xy.X < node._cX ? item._xy.Y < node._cY ? _nextNodeId : _nextNodeId + 3 : item._xy.Y < node._cY ? _nextNodeId + 1 : _nextNodeId + 2;
+                    int nodeId2 = item._xy.X < node._cX ? item._xy.Y < node._cY ? _nextNodeId : _nextNodeId + 3 : item._xy.Y < node._cY ? _nextNodeId + 1 : _nextNodeId + 2;
                     ForceInsert(itemId, nodeId2, item._bounds, item._xy);
                     itemId = item._next;
                 }
                 _nodes[nodeId]._firstChild = _nextNodeId;
-                _nodes[nodeId]._itemCount = -3;
+                _nodes[nodeId]._itemCount = -1;
                 _nextNodeId += 4;
             }
+        }
+
+        public void Update() {
+            foreach (var nIdD in _nodesToGrow) {
+                var nId = nIdD;
+            start:;
+                var n = _nodes[nId];
+                var bounds = Rectangle.Empty;
+                if (n._itemCount < 0) {
+                    var snBounds = _nodes[n._firstChild]._bounds;
+                    if (snBounds.Width != 0)
+                        bounds = snBounds;
+                    snBounds = _nodes[n._firstChild + 1]._bounds;
+                    if (snBounds.Width != 0)
+                        bounds = bounds.Width == 0 ? snBounds : Rectangle.Union(bounds, snBounds);
+                    snBounds = _nodes[n._firstChild + 2]._bounds;
+                    if (snBounds.Width != 0)
+                        bounds = bounds.Width == 0 ? snBounds : Rectangle.Union(bounds, snBounds);
+                    snBounds = _nodes[n._firstChild + 3]._bounds;
+                    if (snBounds.Width != 0)
+                        bounds = bounds.Width == 0 ? snBounds : Rectangle.Union(bounds, snBounds);
+                    if (n._bounds != bounds) {
+                        _nodes[nId]._bounds = bounds;
+                        if (n._parent > -1) {
+                            nId = n._parent;
+                            goto start;
+                        }
+                        continue;
+                    }
+                }
+                if (n._itemCount > 0) {
+                    int l = int.MaxValue,
+                        r = int.MinValue,
+                        t = int.MaxValue,
+                        b = int.MinValue;
+                    var i = _items[n._firstChild];
+                    do {
+                        var bs = i._bounds.AABB;
+                        l = l <= bs.Left ? l : bs.Left;
+                        r = r >= bs.Right ? r : bs.Right;
+                        t = t <= bs.Top ? t : bs.Top;
+                        b = b >= bs.Bottom ? b : bs.Bottom;
+                        if (i._next < 0)
+                            break;
+                        i = _items[i._next];
+                    } while (true);
+                    bounds = new Rectangle(l, t, r - l, b - t);
+                }
+                if (n._bounds != bounds) {
+                    _nodes[nId]._bounds = bounds;
+                    if (n._parent > -1) {
+                        nId = n._parent;
+                        goto start;
+                    }
+                }
+            }
+            _nodesToGrow.Clear();
         }
     }
 }
